703b1dead237bd163fbf5cd872397c82023fad42
Adhithyan <adhithyans34@gmail.com>
2023-05-16 10:39:09 +0530

    ranking added

--- backend/routes/patient.py
+++ backend/routes/patient.py
@@ -176,12 +176,12 @@ async def analysable_unanalysable(id, file: List[UploadFile] = File(...)):
         img1 = nparr1
 
         # neural network
-        model_path = os.path.join('NN_model/new2modelAlexNet-05-0.9267.h5')
+        model_path = os.path.join('NN_model/new_model2.h5')
         model = keras.models.load_model(model_path)
         # img1 = cv2.imread('model_dataset/test/an/sl112_0028.tif')
         font = cv2.FONT_HERSHEY_PLAIN
         img = Image.fromarray(img1, 'RGB')
-        img = img.resize((227,227))
+        img = img.resize((150, 150))
         # print(img)
         img = np.expand_dims(img, axis=0)
         img1 = cv2.resize(img1, (400,400))
@@ -191,51 +191,109 @@ async def analysable_unanalysable(id, file: List[UploadFile] = File(...)):
             new_img = cv2.resize(nparr1, (1000, 1000))
             # Convert to grayscale
             gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-            nparr1 = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-            new_img = cv2.resize(nparr1, (1000, 1000))
-            # Convert to grayscale
-            gray = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-
-            total_area = 0
 
             # Apply threshold
             _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
             # Apply morphological operations
             kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
             cleaned = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
             # cv2.imshow('clean', cleaned)
 
-            # Find contours
-            contours, _ = cv2.findContours(cleaned, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-            min_area = 260  # minimum area for a metaphase chromosome
-            max_area = 8000
+            kernel = np.ones((5, 5), np.uint8)
+            erosion = cv2.erode(cleaned, kernel, iterations=5)
+            # cv2.imshow('erosion', erosion)
+            # Find contours
+            contours, _ = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            min_area = 1000 # minimum area for a nucleus
+            max_area = 1100000
             for cnt in contours:
+                M = cv2.moments(cnt)
                 area = cv2.contourArea(cnt)
             #   area = cv2.contourArea(cnt)
+                radius = int(math.sqrt(area / 3.14)) + 1
                 if area < min_area or area > max_area:
+            #        cv2.circle(cleaned, (cx, cy), radius + 10, (0, 0, 0), -1)
+                    continue
+
+                # Check if the contour is roughly circular using the circularity metric
+                perimeter = cv2.arcLength(cnt, True)
+                circularity = 4 * 3.14 * area / (perimeter ** 2)
+                if circularity > 0.31:
+            #        cv2.drawContours(img, [cnt], 0, (0, 0, 255), 2)
+                    if M['m00']:
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        cv2.circle(cleaned, (cx, cy), radius + 50, (0, 0, 0), -1)
+            #            cv2.circle(img, (cx, cy), 2, (0, 0, 255), -1)
+
+
+            # kernel = np.ones((5, 5), np.uint8)
+            # erosion = cv2.erode(cleaned, kernel, iterations=2)
+            # cv2.imshow('erosion', erosion)
+            contours, _ = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            min_area = 500  # minimum area for a metaphase chromosome
+            max_area = 8000
+            for cnt in contours:
+                M = cv2.moments(cnt)
+                area = cv2.contourArea(cnt)
+            #   area = cv2.contourArea(cnt)
+                radius = int(math.sqrt(area / 3.14)) + 1
+                if area < min_area or area > max_area:
+            #        cv2.circle(cleaned, (cx, cy), radius + 10, (0, 0, 0), -1)
                     continue
 
                 # Check if the contour is roughly circular using the circularity metric
                 perimeter = cv2.arcLength(cnt, True)
                 circularity = 4 * 3.14 * area / (perimeter ** 2)
                 if circularity < 0.74:
-                    # cv2.drawContours(new_img, [cnt], 0, (0, 0, 255), 2)
-                    total_area += area
-            #     print(circularity)
-                # Draw the contour on the original image
+            #        cv2.drawContours(img, [cnt], 0, (0, 0, 255), 2)
+                    if M['m00']:
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        cv2.circle(cleaned, (cx, cy), radius + 50, (0, 0, 0), -1)
+                        cv2.circle(new_img, (cx, cy), 2, (0, 0, 255), -1)
+
+
+            kernel = np.ones((5,5),np.uint8)
+            dilated = cv2.dilate(cleaned,kernel,iterations = 10)
+            # cv2.imshow('dilated', dilated)
+
+            contours, _ = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            min_area = 20000 # minimum area for a nucleus
+            max_area = 1100000
+            for cnt in contours:
+                M = cv2.moments(cnt)
+                area = cv2.contourArea(cnt)
+            #   area = cv2.contourArea(cnt)
+                radius = int(math.sqrt(area / 3.14)) + 1
+                if area < min_area or area > max_area:
+            #        cv2.circle(cleaned, (cx, cy), radius + 10, (0, 0, 0), -1)
+                    continue
+
                 
-            # Draw contours on original image
-            #cv2.drawContours(img, contours, -1, (0,255,0), 3)
+                # Check if the contour is roughly circular using the circularity metric
+                perimeter = cv2.arcLength(cnt, True)
+                circularity = 4 * 3.14 * area / (perimeter ** 2)
+            #    if circularity < 0.74:
+            #        cv2.drawContours(img, [cnt], 0, (0, 0, 255), 2)
+                if M['m00']:
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+            #            cv2.circle(cleaned, (cx, cy), radius + 50, (0, 0, 0), -1)
+            #        cv2.circle(img, (cx, cy), 2, (0, 0, 255), -1)
+            #        cv2.circle(img, (cx, cy), radius+10, (0, 0, 255), 5)
+
+            met_radius = radius + 10
+            total_area = 3.14 * pow(met_radius, 2)
+            patient.AREA.append((new_img, total_area))
 
             # Display the result
-            print(total_area)
-            patient.AREA.append((new_img, total_area))
             # cv2.imshow('Metaphase spread', img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
-            cv2.putText(img1, 'Analysable', (10,40), fontFace= font, fontScale=3, color=(0,255,0), thickness=3)
+            # cv2.putText(img1, 'Analysable', (10,40), fontFace= font, fontScale=3, color=(0,255,0), thickness=3)
             #cv2.imshow('img', img1)
             # plt.imshow(img1)
             print('Analysable')
